9293f7d9dc9bb4bf9152e292cd2b28d508bd2eff
DaniMeschian <daniel.meschian@mentormate.com>
2022-04-05 14:38:18 +0300

    Added additional functionalities in main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello! =)");
+
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
     }
 }
