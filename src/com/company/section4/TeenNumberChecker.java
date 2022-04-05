c5b4f5e99bf0e28211c9ccbb702aeb8c71e27f7c
DaniMeschian <daniel.meschian@mentormate.com>
2022-04-05 17:17:46 +0300

    Added TeenNumberChecker Class

---
+++ src/com/company/section4/TeenNumberChecker.java
@@ -0,0 +1,12 @@
+package com.company.section4;
+
+public class TeenNumberChecker {
+
+    public static void main(String[] args) {
+        System.out.println(isTeen(18));
+    }
+    public static boolean isTeen(int age) {
+        return (age >= 13 && age <= 19);
+    }
+
+}
