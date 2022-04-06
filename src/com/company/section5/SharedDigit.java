7986025ec5f31e6ad423608e1e82cf96c5013acb
DaniMeschian <daniel.meschian@mentormate.com>
2022-04-06 14:35:32 +0300

    Added SharedDigit.java

---
+++ src/com/company/section5/SharedDigit.java
@@ -0,0 +1,16 @@
+package com.company.section5;
+
+public class SharedDigit {
+
+    public static void main(String[] args) {
+        System.out.println(hasSharedDigit(12, 23));
+    }
+
+    public static boolean hasSharedDigit(int x,int y) {
+        if((x > 99) || (x < 10) || (y > 99) || (y < 10)) {
+            return false;
+        }else {
+            return((x/10 == y/10)||(x/10 == y % 10)||(y/10 == x % 10)||(x%10 == y%10));
+        }
+    }
+}
