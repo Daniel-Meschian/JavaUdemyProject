056ee7217b1f06fca1e1ecd8ee1df32aeca23d2c
DaniMeschian <daniel.meschian@mentormate.com>
2022-04-06 14:05:12 +0300

    Added FirstLastDigitSum.java

--- src/com/company/section5/FirstLastDigitSum.java
+++ src/com/company/section5/FirstLastDigitSum.java
@@ -1,2 +1,26 @@
-package com.company.section5;public class FirstLastDigitSum {
+package com.company.section5;
+
+public class FirstLastDigitSum {
+
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+    }
+
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        if (number < 10) {
+            return number * 2;
+        } else {
+            int count = 0;
+            int firstNumber = number;
+            while (firstNumber >= 10) {
+                firstNumber /= 10;
+            }
+            count += firstNumber;
+            count += number % 10;
+            return count;
+        }
+    }
 }
